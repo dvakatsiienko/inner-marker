0aa89b85b07b0f759f4af5285691bdc14518785c
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2025-02-08 23:05:01 +0200

    core: test out useActionState

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,38 @@
-import { useState, memo } from "react";
+/* Core */
+import { useState, memo, useActionState } from "react";
+import waait from "waait";
+
+/* Instruments */
 import "./App.scss";
+
+async function increment(previousState: number, formData: FormData) {
+  console.log("🚀 ~ increment ~ formData:", formData);
+
+  await waait(2000);
+
+  return previousState + 1;
+}
 
 export function App() {
   const [count, setCount] = useState(0);
 
+  const [state, formAction, isPending] = useActionState(increment, 1);
+
   return (
     <main className="min-h-screen grid place-content-center gap-8">
-      <h1 className="text-4xl">Vite / React / TS</h1>
-      <button
-        className="text-9xl text-amber-400"
-        onClick={() => setCount((count) => count + 1)}
-      >
-        count is {count}
-      </button>
+      <h1 className="text-4xl">Vite Foundation</h1>
+
+      <form action={formAction}>
+        <h1 className="text-2xl">Form state: {state}</h1>
+
+        <button
+          disabled={isPending}
+          className="text-9xl text-amber-400 disabled:opacity-50"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          count is {count}
+        </button>
+      </form>
 
       <Button />
       <Button />
@@ -42,5 +62,5 @@ const Button = memo(() => {
 });
 
 const Text = memo(() => {
-  return <p className="text-2xl">Hello, World!</p>;
+  return <h6 className="text-2xl">Hello, World!</h6>;
 });
