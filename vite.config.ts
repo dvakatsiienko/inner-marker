fae2e6ea443f4f3c05826823e39ef1ebe88f468f
Dima Vakatsiienko <imagnum.satellite@gmail.com>
2025-06-25 20:52:31 +0300

    ⚙️ linter: migrate to biome

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
-/* Core */
-
 import vitePluginTailwind from '@tailwindcss/vite';
 import vitePluginReact from '@vitejs/plugin-react-swc';
 import path from 'node:path';
 import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
+// biome-ignore lint/style/noDefaultExport: vite config is exported by default
 export default defineConfig({
-    plugins: [vitePluginReact(), vitePluginTailwind()],
-    resolve: { alias: { '@': path.resolve(__dirname, './src') } },
+  plugins: [vitePluginReact(), vitePluginTailwind()],
+  resolve: { alias: { '@': path.resolve(__dirname, './src') } },
 });
